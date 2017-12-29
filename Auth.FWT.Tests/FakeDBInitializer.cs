54213c1d6e6b64d261be304b96f0b5f2d947a6d5
Andrzej Gołaszewski <agolasz@softserveinc.com>
2017-12-29 18:29:57 +0100

    test project

---
+++ Auth.FWT.Tests/FakeDBInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+
+namespace Auth.FWT.Tests
+{
+    public class FakeDBInitializer : DropCreateDatabaseAlways<FakeContext>
+    {
+        public FakeDBInitializer() : base()
+        {
+        }
+
+        public override void InitializeDatabase(FakeContext context)
+        {
+            base.InitializeDatabase(context);
+        }
+
+        protected override void Seed(FakeContext context)
+        {
+            base.Seed(context);
+        }
+    }
+}
