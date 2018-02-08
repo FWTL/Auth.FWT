c98f8895af1e0aa292778a2c9f3a8fb2be893e2c
Andrzej Gołaszewski <agolasz@softserveinc.com>
2018-02-08 10:28:09 +0100

    azure project init

---
+++ Auth.FWT._Azure/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auth.FWT._Azure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
