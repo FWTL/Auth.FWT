72c99dab9ab600c4d2d8aff79e71dfcef98fa6a3
Andrzej Gołaszewski <golaszewski.a@gmail.com>
2018-12-24 00:22:31 +0100

    user id instead of phoneHashId

--- FWT.Core/Const.cs
+++ FWT.Core/Const.cs
@@ -2,6 +2,6 @@
 {
     public static class Const
     {
-        public static readonly string PHONE_HASH_ID = "PhoneHashId";
+        public static readonly string USER_ID = "UserId";
     }
 }
--- FWT.Infrastructure/IdentityServer/TelegramProfileService.cs
+++ FWT.Infrastructure/IdentityServer/TelegramProfileService.cs
@@ -10,10 +10,10 @@ namespace FWTL.Infrastructure.IdentityServer
     {
         public Task ValidateAsync(CustomTokenRequestValidationContext context)
         {
-            string phoneHashId = context.Result.ValidatedRequest.Raw[Const.PHONE_HASH_ID];
-            if (phoneHashId.IsNotNull())
+            string userId = context.Result.ValidatedRequest.Raw[Const.USER_ID];
+            if (userId.IsNotNull())
             {
-                context.Result.ValidatedRequest.ClientClaims.Add(new Claim(Const.PHONE_HASH_ID, phoneHashId));
+                context.Result.ValidatedRequest.ClientClaims.Add(new Claim(Const.USER_ID, userId));
             }
 
             return Task.CompletedTask;
